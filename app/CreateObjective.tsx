edbf77bd23b96b0b6d6b1ad78789e39b388465de
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-07-14 07:41:14 +0200

    validation

--- app/CreateObjective.tsx
+++ app/CreateObjective.tsx
@@ -192,6 +192,12 @@ export default function Form({ onSubmit }: ObjectiveProps) {
     };
 
     const handleSubmit = async () => {
+        // validation for an error
+        if (typeof onSubmit !== "function") {
+            console.error("onSubmit is not a function");
+            return;
+        }
+
         const formData: ObjectiveWithoutId = {
             exercise,
             days,
