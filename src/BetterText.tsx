7289b88ec544d34f281e028c6926675c5f50d16e
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-07-20 14:36:10 +0200

    Test if JSDoc works

--- src/BetterText.tsx
+++ src/BetterText.tsx
@@ -5,8 +5,41 @@ import React, { ReactNode } from "react";
 import { TextStyle, Text } from "react-native";
 
 // TypeScript, supongo
+/**
+ * BetterTextProps interface
+ *
+ * @interface BetterTextProps
+ * @typedef {BetterTextProps}
+ */
 interface BetterTextProps {
+    /**
+     * The size of the text in px. No default, required.
+     *
+     * @type {number}
+     */
     fontSize: number; // Tamaño de letra en px
+    /**
+     * The weight of the text. No default, required. Some of them won't be supported in the case of `isSerif` being true.
+     *
+     * @type {(| "Light"
+     *         | "LightItalic"
+     *         | "ExtraLight"
+     *         | "ExtraLightItalic"
+     *         | "Thin"
+     *         | "ThinItalic"
+     *         | "Regular"
+     *         | "Italic"
+     *         | "Medium"
+     *         | "MediumItalic"
+     *         | "SemiBold"
+     *         | "SemiBoldItalic"
+     *         | "Bold"
+     *         | "BoldItalic"
+     *         | "ExtraBold"
+     *         | "ExtraBoldItalic"
+     *         | "Black"
+     *         | "BlackItalic")}
+     */
     fontWeight:
         | "Light"
         | "LightItalic"
@@ -26,15 +59,60 @@ interface BetterTextProps {
         | "ExtraBoldItalic"
         | "Black"
         | "BlackItalic"; // Intensidad de la tipografía
+    /**
+     * The color of the text, as a HEX string (e.g. "#FFF", "#C8C8C8"). Defaults to white.
+     *
+     * @type {?string}
+     */
     textColor?: string; // Color del texto - default: blanco (ya que el fondo es negro)
+    /**
+     * Wether to use the Noto Serif serif font. Defaults to false. Beware, as if you use `isSerif` with an incompatible `fontWeight` value you might stumble onto text not rendering properly.
+     *
+     * @type {?boolean}
+     */
     isSerif?: boolean; // Wether to use Serif font or not
+    /**
+     * A void function that will be executed upon touching the text. Useful for URL links.
+     *
+     * @type {?() => void}
+     */
     onTap?: () => void; // Si se proporciona, el texto actuará como un botón / enlace y hará algo
+    /**
+     * How to align the text. Defaults to "left". "left" and "center" are equivalent.
+     *
+     * @type {?("center" | "normal" | "left" | "right")}
+     */
     textAlign?: "center" | "normal" | "left" | "right"; // Alineación del texto
+    /**
+     * The text to be rendered.
+     *
+     * @type {ReactNode}
+     */
     children: ReactNode; // Texto en sí, dentro del tag <BeText>
+    /**
+     * If true, the text will be styled as a URL (underline and color, basically).
+     *
+     * @type {?boolean}
+     */
     url?: boolean; // Si es verdadero el texto se estilizará como un enlace externo - default: false
 }
 
 // Creamos la función
+/**
+ * Homemade text component
+ *
+ * @export
+ * @param {BetterTextProps} param0
+ * @param {number} param0.fontSize
+ * @param {("Light" | "LightItalic" | "ExtraLight" | "ExtraLightItalic" | "Thin" | "ThinItalic" | "Regular" | "Italic" | "Medium" | "MediumItalic" | "SemiBold" | "SemiBoldItalic" | "Bold" | ... 4 more ... | "BlackItalic")} param0.fontWeight
+ * @param {ReactNode} param0.children
+ * @param {string} param0.textColor
+ * @param {("center" | "normal" | "left" | "right")} param0.textAlign
+ * @param {() => void} param0.onTap
+ * @param {boolean} param0.isSerif
+ * @param {boolean} param0.url
+ * @returns {*}
+ */
 export default function BetterText({
     fontSize,
     fontWeight,
--- src/Buttons.tsx
+++ src/Buttons.tsx
@@ -6,17 +6,72 @@ import { Pressable, DimensionValue } from "react-native";
 import BetterText from "@/src/BetterText";
 
 // TypeScript, supongo
+/**
+ * ButtonProps interface
+ *
+ * @interface BtnsProps
+ * @typedef {BtnsProps}
+ */
 interface BtnsProps {
+    /**
+     * The style of the button. Use ACE (blue) for accent, GOD (green) for positive actions, WOR (red) for destructive actions, HMM (orange) for warning buttons, and DEFAULT (grey-black) for extra actions.
+     *
+     * @type {("ACE" | "GOD" | "WOR" | "DEFAULT" | "HMM")}
+     */
     style: "ACE" | "GOD" | "WOR" | "DEFAULT" | "HMM";
+    /**
+     * The text of the button. Can also be passed as children (ReactNode), but if you prefer to use this property you can.
+     *
+     * @type {?string}
+     */
     buttonText?: string;
+    /**
+     * A void function that the button will perform upon press.
+     *
+     * @type {() => void}
+     */
     action: () => void;
+    /**
+     * Width of the button. Defaults to auto. Fill will use CSS "100%", while auto will use CSS "auto".
+     *
+     * @type {?("auto" | "fill")}
+     */
     width?: "auto" | "fill";
+    /**
+     * Height of the button. Defaults to auto. Fill will use CSS "100%", while auto will use CSS "auto". Default will use a fixed number (55). You can also pass a custom fixed number if needed.
+     *
+     * @type {?("auto" | "fill" | "default" | number)}
+     */
     height?: "auto" | "fill" | "default" | number;
+    /**
+     * The layout of the button. Defaults to normal. Box will override height & width, override CSS flex: 1 directive and make the button a 50x50 box. Fixed will also override height and flex, but not width.
+     *
+     * @type {?("normal" | "box" | "fixed")}
+     */
     layout?: "normal" | "box" | "fixed";
+    /**
+     * The text of the button, as children.
+     *
+     * @type {?ReactNode}
+     */
     children?: ReactNode;
 }
 
 // Creamos la funcion
+/**
+ * Homemade button component.
+ *
+ * @export
+ * @param {BtnsProps} param0
+ * @param {("ACE" | "GOD" | "WOR" | "DEFAULT" | "HMM")} param0.style The style of the button. Defaults to default. ACE = blue, GOD = green, WOR = red, HMM = orange, and DEFAULT = black.
+ * @param {string} param0.buttonText The text of the button. Can be passed either here or as the children.
+ * @param {() => void} param0.action What shall the button do upon press.
+ * @param {("auto" | "fill")} param0.width The width of the button
+ * @param {(number | "auto" | "fill" | "default")} param0.height The height of the button.
+ * @param {("normal" | "box" | "fixed")} param0.layout The layout. Defaults to normal.
+ * @param {ReactNode} param0.children The text of the button. Can be passed either here or as a parameter.
+ * @returns {*}
+ */
 export default function Button({
     style,
     buttonText,
