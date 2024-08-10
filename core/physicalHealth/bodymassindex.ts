6dab3868e439ec0073e9af3ec621cf05d31396ff
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-08-11 00:17:35 +0200

    SEND HELP

--- core/physicalHealth/bodymassindex.ts
+++ core/physicalHealth/bodymassindex.ts
@@ -90,13 +90,19 @@ interface BMIPercentiles {
 // into separate objects
 
 // note:
-// since i only have a visual graph and not a table, there is a very slight possible error margin (i have vision problems alright?). the error margin is of a max of 0.2
+// since i only have a visual graph (see SOURCES) and not a table, there is a very slight possible error margin (i have vision problems alright?). the error margin is of a max of 0.2
 // as seen in documentation (testing section) the default valid error margin for health calculations (like this one) is of 0.3, hence there shouldnt be any big problem, dont worry if you see ive put "15.76" and for you that point of the graph looks like "15.69", for example
+
+// edit regarding the above: nevermind, i did get a table.
+// TODO: revise all data to adjust its precision
+// TODO: update comments
+// TODO: add female percentiles
+// TODO: move this to separate component for code readability
 
 // (send help)
 const male_percentiles: BMIPercentiles = {
     2: {
-        5: 14,
+        5: 14.73,
         10: 15.1,
         25: 15.76,
         50: 16.60,
@@ -105,7 +111,186 @@ const male_percentiles: BMIPercentiles = {
         90: 18.60,
         95: 19.35
     },
-    // TODO
+    3: {
+        5: 14.375,
+        10: 14.635,
+        25: 15.34,
+        50: 16,
+        75: 16.81,
+        85: 17.38,
+        90: 17.66,
+        95: 18.205
+    },
+    4: {
+        5: 14,
+        10: 14.395,
+        25: 14.96,
+        50: 15.61,
+        75: 16.42,
+        85: 16.98,
+        90: 17.26,
+        95: 17.81
+    },
+    5: {
+        5: 13.91,
+        10: 14.18,
+        25: 14.66,
+        50: 15.40,
+        75: 16.25,
+        85: 16.805,
+        90: 17.21,
+        95: 17.96
+    },
+    6: {
+        5: 13.78,
+        10: 14.01,
+        25: 14.60,
+        50: 15.40,
+        75: 16.38,
+        85: 17,
+        90: 17.45,
+        95: 18.40
+    },
+    7: {
+        5: 13.75,
+        10: 14.03,
+        25: 14.62,
+        50: 15.45,
+        75: 16.60,
+        85: 17.39,
+        90: 18,
+        95: 19.05,
+    },
+    8: {
+        5: 13.79,
+        10: 14.15,
+        25: 14.83,
+        50: 15.78,
+        75: 17.05,
+        85: 17.95,
+        90: 18.69,
+        95: 20.06
+    },
+    9: {
+        5: 13.96,
+        10: 14.35,
+        25: 15.10,
+        50: 16.16,
+        75: 17.60,
+        85: 18.63,
+        90: 19.48,
+        95: 21.08
+    },
+    10: {
+        5: 14.21,
+        10: 14.64,
+        25: 15.47,
+        50: 16.64,
+        75: 18.25,
+        85: 19.40,
+        90: 20.34,
+        95: 22.15
+    },
+    11: {
+        5: 14.56,
+        10: 15.02,
+        25: 15.92,
+        50: 17.20,
+        75: 18.94,
+        85: 20.19,
+        90: 21.23,
+        95: 23.21
+    },
+    12: {
+        5: 14.97,
+        10: 15.47,
+        25: 16.44,
+        50: 17.81,
+        75: 19.68,
+        85: 21.02,
+        90: 22.13,
+        95: 24.22
+    },
+    13: {
+        5: 15.45,
+        10: 15.98,
+        25: 17.01,
+        50: 18.47,
+        75: 20.44,
+        85: 21.85,
+        90: 23,
+        95: 25.17
+    },
+    14: {
+        5: 16,
+        10: 16.46,
+        25: 17.6,
+        50: 19.05,
+        75: 21.20,
+        85: 22.60,
+        90: 24.80,
+        95: 26,
+    },
+    15: {
+        5: 16.55,
+        10: 17.13,
+        25: 18.27,
+        50: 19.85,
+        75: 21.97,
+        85: 23.45,
+        90: 24.64,
+        95: 26.83
+    },
+    16: {
+        5: 17.13,
+        10: 17.73,
+        25: 18.91,
+        50: 20.55,
+        75: 22.72,
+        85: 24.21,
+        90: 25.40,
+        95: 27.56
+    },
+    17: {
+        5: 17.7,
+        10: 18.33,
+        25: 19.55,
+        50: 21.24,
+        75: 23.44,
+        85: 24.94,
+        90: 26.13,
+        95: 28.25
+    },
+    18: {
+        5: 18.20,
+        10: 19.83,
+        25: 20.10,
+        50: 21.85,
+        75: 24.10,
+        85: 25.60,
+        90: 26.80,
+        95: 28.925,
+    },
+    19: {
+        5: 18.73,
+        10: 19.40,
+        25: 20.69, // nice
+        50: 22.44,
+        75: 24.80,
+        85: 26.36,
+        90: 27.58,
+        95: 29.62
+    },
+    20: {
+        5: 19.05,
+        10: 19.80,
+        25: 21.18,
+        50: 23,
+        75: 25.4025, // precise asf
+        85: 27,
+        90: 28.24,
+        95: 30.59
+    }
 }
 
 const female_percentiles: BMIPercentiles = {
