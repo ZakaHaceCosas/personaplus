957d63999a648df8bc1b001cb489945dbf962508
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-08-12 19:03:29 +0200

    Move functions inside the main function, so less params are needed and code is simpler

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 // index.tsx
 // Welcome to PersonaPlus. Give yourself a plus!
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, ReactElement } from "react";
 import {
     View,
     ScrollView,
@@ -38,11 +38,11 @@ import { adjustedToday } from "@/src/toolkit/today";
 import * as TaskManager from "expo-task-manager";
 import colors from "@/src/toolkit/design/colors";
 import Loading from "@/src/Loading";
+import generateRandomMessage from "@/src/toolkit/design/randomMessages";
 
 // TypeScript, supongo
 import { Objective } from "@/src/types/Objective";
 import { TFunction } from "i18next";
-import generateRandomMessage from "@/src/toolkit/design/randomMessages";
 
 // We define the styles
 const styles = StyleSheet.create({
@@ -59,96 +59,6 @@ const styles = StyleSheet.create({
     },
 });
 
-// To prevent code from being super nested / unreadable, I've moved some things here, so they act as separate components
-function AllObjectivesDone(t: TFunction, randomMessageForAllDone: string) {
-    return (
-        <View
-            style={{
-                padding: 20,
-                flex: 1,
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-            }}
-        >
-            <BetterText
-                textAlign="center"
-                fontSize={30}
-                textColor={colors.BASIC.WHITE}
-                fontWeight="Bold"
-            >
-                {t("page_home.no_objectives.all_done")}
-            </BetterText>
-            <GapView height={10} />
-            <BetterText
-                textAlign="center"
-                fontSize={15}
-                textColor={colors.BASIC.WHITE}
-                fontWeight="Regular"
-            >
-                {randomMessageForAllDone}
-            </BetterText>
-        </View>
-    );
-}
-
-function ObjectiveDivision(
-    obj: Objective,
-    t: TFunction,
-    start: (arg0: number) => void,
-    markAsDone: (arg0: number) => void
-) {
-    return (
-        <Division
-            key={obj.identifier}
-            status="REGULAR"
-            preheader={t("sections.divisions.active_objective")}
-            header={t(`globals.supported_active_objectives.${obj.exercise}`)}
-        >
-            <Button
-                style="ACE"
-                action={() => start(obj.identifier)}
-                buttonText={t("globals.lets_go")}
-            />
-            <Button
-                style="GOD"
-                action={() => markAsDone(obj.identifier)}
-                buttonText={t("globals.already_done_it")}
-            />
-        </Division>
-    );
-}
-
-function NoObjectives(t: TFunction, create: () => void) {
-    return (
-        <View
-            style={{
-                padding: 20,
-                flex: 1,
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-            }}
-        >
-            <BetterText
-                textAlign="center"
-                fontSize={30}
-                textColor={colors.BASIC.WHITE}
-                fontWeight="Bold"
-            >
-                {t("page_home.no_objectives.not_any_objective")}
-            </BetterText>
-            <GapView height={15} />
-            <Button
-                width="fill"
-                style="ACE"
-                buttonText={t("globals.lets_go")}
-                action={create}
-            />
-        </View>
-    );
-}
-
 // We create the function
 export default function Home() {
     const [loading, setLoading] = useState(true);
@@ -164,8 +74,6 @@ export default function Home() {
     const { t } = useTranslation();
     const currentpage = usePathname();
 
-    const [randomMessage, setRandomMessage] = useState<string>();
-
     // marking an objective as done
     const handleMarkingObjectiveAsDone = async (identifier: number) => {
         try {
@@ -180,6 +88,115 @@ export default function Home() {
         }
     };
 
+    // To prevent code from being super nested / unreadable, I've moved some things here, so they act as separate components
+    /**
+     * Generates an "All objectives done!" view. Use it inside of the `<Section kind="objectives">`, for when there are objectives for today but are all done.
+     *
+     * @param {TFunction} t Pass the translate function, please.
+     * @returns {*}
+     */
+    function AllObjectivesDone(t: TFunction) {
+        const message: string = generateRandomMessage("all_done", t);
+
+        return (
+            <View
+                style={{
+                    padding: 20,
+                    flex: 1,
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+                <BetterText
+                    textAlign="center"
+                    fontSize={30}
+                    textColor={colors.BASIC.WHITE}
+                    fontWeight="Bold"
+                >
+                    {t("page_home.no_objectives.all_done")}
+                </BetterText>
+                <GapView height={10} />
+                <BetterText
+                    textAlign="center"
+                    fontSize={15}
+                    textColor={colors.BASIC.WHITE}
+                    fontWeight="Regular"
+                >
+                    {message}
+                </BetterText>
+            </View>
+        );
+    }
+
+    /**
+     * Renders a `<Division>` for a given objective that's due today.
+     *
+     * @param {Objective} obj The `Objective`
+     * @param {TFunction} t Pass here the translate function, please.
+     * @returns {ReactElement} A JSX element (a `<Division>`).
+     */
+    function ObjectiveDivision(obj: Objective, t: TFunction): ReactElement {
+        return (
+            <Division
+                key={obj.identifier}
+                status="REGULAR"
+                preheader={t("sections.divisions.active_objective")}
+                header={t(
+                    `globals.supported_active_objectives.${obj.exercise}`
+                )}
+            >
+                <Button
+                    style="ACE"
+                    action={() => startSessionFromObjective(obj.identifier)}
+                    buttonText={t("globals.lets_go")}
+                />
+                <Button
+                    style="GOD"
+                    action={() => handleMarkingObjectiveAsDone(obj.identifier)}
+                    buttonText={t("globals.already_done_it")}
+                />
+            </Division>
+        );
+    }
+
+    /**
+     * Shows a header and a button, stating that there are no objectives. Use it when there are no created objectives at all.
+     *
+     * @param {TFunction} t Pass here the translate function, please.
+     * @param {() => void} create A void function to create an objective.
+     * @returns {ReactElement} A JSX element.
+     */
+    function NoObjectives(t: TFunction, create: () => void): ReactElement {
+        return (
+            <View
+                style={{
+                    padding: 20,
+                    flex: 1,
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+                <BetterText
+                    textAlign="center"
+                    fontSize={30}
+                    textColor={colors.BASIC.WHITE}
+                    fontWeight="Bold"
+                >
+                    {t("page_home.no_objectives.not_any_objective")}
+                </BetterText>
+                <GapView height={15} />
+                <Button
+                    width="fill"
+                    style="ACE"
+                    buttonText={t("globals.lets_go")}
+                    action={create}
+                />
+            </View>
+        );
+    }
+
     function renderObjectiveDivision(obj: Objective) {
         if (
             obj &&
@@ -188,21 +205,10 @@ export default function Home() {
             dueTodayObjectiveList.includes(obj.identifier)
         ) {
             termLog("Rendering Objective Division:" + obj, "log");
-            return ObjectiveDivision(
-                obj,
-                t,
-                startSessionFromObjective,
-                handleMarkingObjectiveAsDone
-            );
+            return ObjectiveDivision(obj, t);
         }
         return null;
     }
-
-    useEffect(() => {
-        const randomMessageForAllDone = generateRandomMessage("all_done", t);
-        setRandomMessage(randomMessageForAllDone);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
 
     useEffect(() => {
         // Verifies background fetching status and logs it
@@ -376,7 +382,7 @@ export default function Home() {
                             renderObjectiveDivision(objectives[key])
                         );
                     } else {
-                        return AllObjectivesDone(t, randomMessage ?? "Error.");
+                        return AllObjectivesDone(t);
                     }
                 } else {
                     return NoObjectives(t, createNewActiveObjective);
