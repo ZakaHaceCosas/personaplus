1286b0cf49db8e49f28306d6a4eb9a4b29d2ad4e
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-08-14 14:37:16 +0200

    Add JSDoc to randomMessages

--- src/toolkit/design/randomMessages.ts
+++ src/toolkit/design/randomMessages.ts
@@ -1,13 +1,21 @@
 import { TFunction } from "i18next";
 
-export default function generateRandomMessage(target: "all_done" | "session_done" | "reminders", t: TFunction) {
+/**
+ * For the app to be more friendly, generates a random message for certain parts of the UI.
+ *
+ * @export
+ * @param {("all_done" | "session_done" | "reminders")} target What kind of random message you're seeking. It uses the name of the `translations/*.json` files, e.g. "`all_done`" will return a random message from `cool_messages.all_done`.
+ * @param {TFunction} t Pass here the translate function, please.
+ * @returns {string} Returns a string with your message.
+ */
+export default function generateRandomMessage(target: "all_done" | "session_done" | "reminders", t: TFunction): string {
     // choose a random message for certain parts of the UI
     // so the app feels more friendly :D
     const allMessages: string[] = t(`cool_messages.${target}`, {
         returnObjects: true,
     });
 
-    const message =
+    const message: string =
         allMessages[Math.floor(Math.random() * allMessages.length)];
 
     return message
