842cdcae7aae716f374d095821a861d161c8608c
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-08-22 22:15:02 +0200

    please work asdksadfjduogfhewyigi

--- babel.config.js
+++ babel.config.js
@@ -7,7 +7,7 @@ module.exports = function (api) {
                 "module-resolver",
                 {
                     alias: {
-                        "@": "./*",
+                        "@": "./",
                     },
                 },
             ],
