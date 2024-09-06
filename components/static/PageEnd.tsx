993c6eb0639bf39e505b8f75c6921862bceed190
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-09-06 19:27:28 +0200

    Add page end

---
+++ components/static/PageEnd.tsx
@@ -0,0 +1,47 @@
+import { ReactElement } from "react";
+import { StyleSheet, View } from "react-native";
+import { useTranslation } from "react-i18next";
+import BetterText from "@/components/text/BetterText";
+import FontSizes from "@/constants/FontSizes";
+import Colors from "@/constants/Colors";
+import getCommonScreenSize from "@/constants/Screen";
+
+interface PageEndProps {
+    includeText: boolean;
+}
+
+const styles = StyleSheet.create({
+    pageEnd: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        width: getCommonScreenSize("width"),
+    },
+});
+
+export default function PageEnd({ includeText }: PageEndProps): ReactElement {
+    const { t } = useTranslation();
+
+    return (
+        <View
+            style={[
+                styles.pageEnd,
+                {
+                    height: includeText ? 180 : 160,
+                    marginTop: includeText ? 20 : 0,
+                },
+            ]}
+        >
+            {includeText && (
+                <BetterText
+                    textAlign="center"
+                    fontWeight="Regular"
+                    fontSize={FontSizes.REGULAR}
+                    textColor={Colors.LBLS.SDD}
+                >
+                    {t("hola chavales")}
+                </BetterText>
+            )}
+        </View>
+    );
+}
