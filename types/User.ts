b84b5107379646d4ef24f86edce40e45c13f321c
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-09-08 16:55:59 +0200

    Update User types to better validate unvalid data

--- types/User.ts
+++ types/User.ts
@@ -6,10 +6,11 @@ export interface FullProfile {
     gender: "male" | "female" | null;
     language: "es" | "en";
     activness: "poor" | "small" | "normal" | "intense" | "super" | null;
-    focus: "noPriority" | "assistMePls" | "wellbeing" | "diet" | "exercising" | null;
+    focus: "noPriority" | "assistMePls" | "wellbeing" | "diet" | "exercising" | "" | null; // both "" and null serve as a "null" / invalid value
     sleepHours: 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | "";
     theThinkHour: string,
     isNewUser: boolean,
 }
 
-export type BasicUserData = Omit<FullProfile, "focus" | "sleepHours" | "activness" | "language">; export type BasicUserHealthData = Omit<BasicUserData, "username">;
+export type BasicUserData = Omit<FullProfile, "focus" | "sleepHours" | "activness" | "language">;
+export type BasicUserHealthData = Omit<BasicUserData, "username">;
