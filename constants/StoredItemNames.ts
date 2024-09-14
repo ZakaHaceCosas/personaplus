5650e6915a36b76226f39e844f1efb7769bc1501
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-09-14 13:38:33 +0200

    Remove deprecated item name

--- constants/StoredItemNames.ts
+++ constants/StoredItemNames.ts
@@ -1,14 +1,14 @@
 /**
  * The name of each AsyncStorage entry, so you don't mess up.
  *
- * @type {{ userData: string; objectives: string; dailyLog: string; consoleLogs: string; lang: string; }}
+ * @type {{ userData: string; objectives: string; dailyLog: string; consoleLogs: string; }}
  */
 const StoredItemNames = {
     userData: "userData",
     objectives: "objectives",
     dailyLog: "activeObjectiveDailyLog",
     consoleLogs: "globalLogs",
-    lang: "language"
+    colorTheme: "colorTheme"
 }
 
 export default StoredItemNames
