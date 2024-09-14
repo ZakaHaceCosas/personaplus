3c761df171c33e7ba9ac8ab92a938b939924d2d7
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-09-14 13:38:03 +0200

    Update to R6 rewrite (pt 14) - Fix language handler (AsyncStorage items were wrongly named), improved typing, added try-catch blocks and tracebacking

--- translations/translate.ts
+++ translations/translate.ts
@@ -2,22 +2,29 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 // somehow unused: import { getLocales } from 'expo-localization';
-
 import enTranslations from '@/translations/en.json';
 import esTranslations from '@/translations/es.json';
 import { FullProfile } from '@/types/User';
 import { logToConsole } from '@/toolkit/debug/Console';
+import StoredItemNames from '@/constants/StoredItemNames';
 
-// Define las traducciones disponibles
+// Defines available translates
 const resources = {
     en: { translation: enTranslations },
     es: { translation: esTranslations },
 };
 
+/**
+ * Gets the locale that is currently being used, or the default one if none.
+ *
+ * @export
+ * @async
+ * @returns {Promise<"es" | "en">}
+ */
 export async function getDefaultLocale(): Promise<"es" | "en"> {
     try {
         // const locales = getLocales();     (unused, for whatever reason)
-        const savedData = await AsyncStorage.getItem('userData');
+        const savedData: string | null = await AsyncStorage.getItem(StoredItemNames.userData);
         let savedDataValue: FullProfile | null = null;
 
         try {
@@ -27,42 +34,89 @@ export async function getDefaultLocale(): Promise<"es" | "en"> {
                 logToConsole("Warning! No language data. Might fallback to english.", "warn")
             }
         } catch (e) {
-            logToConsole("Error parsing JSON from AsyncStorage: " + e, "warn");
+            logToConsole("Error parsing JSON from AsyncStorage: " + e, "warn", {
+                location: "@/translations/translate.ts",
+                isHandler: false,
+                function: "getDefaultLocale() @ try-catch #1 @ sub try-catch #1"
+            }, false);
         }
 
-        const savedLanguageValue = savedDataValue?.language ?? "es"; // TODO: change to "en" upon release
+        const savedLanguageValue: "es" | "en" = savedDataValue?.language ?? "es"; // TODO: change to "en" upon release
         const defaultLanguage: "es" | "en" = (savedLanguageValue === "es" || savedLanguageValue === "en") ? savedLanguageValue : "en";
 
         return defaultLanguage;
     } catch (e) {
-        logToConsole("Error handling getDefaultLocale(): " + e + ". This is a warning and not an error because it doesn't have severe side effects. Fallback to English.", "warn")
+        logToConsole("Error handling getDefaultLocale(): " + e + ". This is a warning and not an error because it doesn't have severe side effects. Fallback to English.", "warn", {
+            location: "@/translations/translate.ts",
+            isHandler: false,
+            function: "getDefaultLocale() @ try-catch #1"
+        })
         return "en"
     }
 }
 
-// Función para inicializar i18n
-const initializeI18n = async () => {
-    const defaultLanguage = await getDefaultLocale()
+/**
+ * Function to initialize i18n
+ *
+ * @async
+ * @returns {Promise<void>}
+ */
+async function initializeI18n(): Promise<void> {
+    const defaultLanguage: "es" | "en" = await getDefaultLocale()
 
-    // Inicializa i18n con los recursos y el idioma detectado
-    i18n.use(initReactI18next).init({
-        resources,
-        lng: defaultLanguage,
-        fallbackLng: 'en',
-        interpolation: {
-            escapeValue: false,
-        },
-        compatibilityJSON: 'v3',
-    });
+    // try to init i18n with detected language and resources
+    try {
+        i18n
+            .use(initReactI18next)
+            .init({
+                resources,
+                lng: defaultLanguage,
+                fallbackLng: 'en',
+                interpolation: {
+                    escapeValue: false,
+                },
+                compatibilityJSON: 'v3',
+            });
+    } catch (e) {
+        logToConsole(
+            "Error with i18n init: " + e,
+            "error",
+            {
+                location: "@/translations/translate.ts",
+                isHandler: false,
+                function: "initializeI18n()"
+            }
+        )
+    }
 };
 
-// Llama a la función para inicializar i18n
 initializeI18n();
 
-// Función para cambiar el idioma y guardarlo en AsyncStorage
-const changeLanguage = async (language: string) => {
-    await AsyncStorage.setItem('language', language);
-    i18n.changeLanguage(language);
+/**
+ * Function to change the language and save it to the AsyncStorage. **Async function.**
+ *
+ * @async
+ * @param {("es" | "en")} language The language you want to set.
+ * @returns {Promise<0 | 1>} 0 if success, 1 if failure.
+ */
+async function changeLanguage(language: "es" | "en"): Promise<0 | 1> {
+    try {
+        const savedData: string | null = await AsyncStorage.getItem(StoredItemNames.userData);
+        if (savedData === null) {
+            throw new Error("Why is userData null?")
+        }
+        const userData: FullProfile = JSON.parse(savedData)
+        await AsyncStorage.setItem(StoredItemNames.userData, JSON.stringify({ ...userData, language }));
+        i18n.changeLanguage(language);
+        return 0
+    } catch (e) {
+        logToConsole("Error changing language: " + e, "error", {
+            function: "changeLanguage",
+            isHandler: false,
+            location: "@/translations/translate.ts"
+        }, false)
+        return 1
+    }
 };
 
 export default i18n;
