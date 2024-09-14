8eddbd59247d125eb8e936b2abf3d5680cdbf81d
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-09-14 12:17:16 +0200

    ome code formatting

--- types/User.ts
+++ types/User.ts
@@ -6,11 +6,21 @@ export interface FullProfile {
     gender: "male" | "female" | null;
     language: "es" | "en";
     activness: "poor" | "small" | "normal" | "intense" | "super" | null;
-    focus: "noPriority" | "assistMePls" | "wellbeing" | "diet" | "exercising" | "" | null; // both "" and null serve as a "null" / invalid value
+    focus:
+    | "noPriority"
+    | "assistMePls"
+    | "wellbeing"
+    | "diet"
+    | "exercising"
+    | ""
+    | null; // both "" and null serve as a "null" / invalid value
     sleepHours: 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | "";
-    theThinkHour: string,
-    isNewUser: boolean,
+    theThinkHour: string;
+    isNewUser: boolean;
 }
 
-export type BasicUserData = Omit<FullProfile, "focus" | "sleepHours" | "activness" | "language">;
+export type BasicUserData = Omit<
+    FullProfile,
+    "focus" | "sleepHours" | "activness" | "language"
+>;
 export type BasicUserHealthData = Omit<BasicUserData, "username">;
