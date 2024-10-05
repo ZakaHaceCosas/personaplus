639c7127894c4997a7ad7f8d5c57fda62d72808e
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-10-05 12:32:36 +0200

    Update to R6 rewrite (pt 19 (+pt 16) - Finally get objectives to display correctly and update upon changes in the home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import BetterButton from "@/components/interaction/BetterButton";
 import Loading from "@/components/static/Loading";
 import PageEnd from "@/components/static/PageEnd";
 import {
@@ -5,11 +6,21 @@ import {
     BetterTextSubHeader,
 } from "@/components/text/BetterTextPresets";
 import GapView from "@/components/ui/GapView";
+import Division from "@/components/ui/sections/Division";
 import Section from "@/components/ui/sections/Section";
-import { RenderActiveObjectives } from "@/components/ui/sections/interface/Home";
 import { orchestrateUserData } from "@/toolkit/User";
 import { logToConsole } from "@/toolkit/debug/Console";
+import {
+    GetActiveObjective,
+    GetAllPendingObjectives,
+    SaveActiveObjectiveToDailyLog,
+} from "@/toolkit/objectives/ActiveObjectives";
+import {
+    ActiveObjective,
+    SupportedActiveObjectives,
+} from "@/types/ActiveObjectives";
 import { FullProfile } from "@/types/User";
+import { router } from "expo-router";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
@@ -17,25 +28,109 @@ export default function HomeScreen() {
     const { t } = useTranslation();
     const [userData, setUserData] = useState<FullProfile | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
+    const [renderedObjectives, setRenderedObjectives] = useState<
+        ActiveObjective[] | null
+    >(null);
+    const [identifiers, setIdentifiers] = useState<number[] | false | 0 | null>(
+        null
+    );
+
+    async function fetchObjectives(): Promise<void> {
+        try {
+            const pending: number[] | 0 | false | null =
+                await GetAllPendingObjectives();
+            setIdentifiers(pending);
+
+            if (!Array.isArray(pending)) {
+                if (!pending || pending === 0 || pending === false) return;
+            }
+
+            const objectives = await Promise.all(
+                pending.map((identifier) => GetActiveObjective(identifier))
+            );
+
+            const filteredObjectives = objectives.filter(
+                (obj): obj is ActiveObjective => obj !== null
+            );
+            setRenderedObjectives(filteredObjectives);
+        } catch (e) {
+            logToConsole(
+                "Error rendering objectives: " + e,
+                "error",
+                {
+                    location: "@/components/ui/sections/interface/Home.tsx",
+                    isHandler: true,
+                    handlerName: "renderObjectives() @ try-catch #1",
+                    function: "RenderActiveObjectives()",
+                },
+                false
+            );
+        }
+    }
+
+    async function fetchUserData(): Promise<void> {
+        try {
+            const response = await orchestrateUserData();
+            setUserData(response);
+        } catch (e) {
+            logToConsole("Error accessing user data! " + e, "error", {
+                location: "@/app/(tabs)/index.tsx",
+                function: "fetchUserData()",
+                isHandler: false,
+            });
+        }
+    }
+
+    async function handler() {
+        try {
+            await fetchUserData();
+            await fetchObjectives();
+        } catch (e) {
+            throw e;
+        } finally {
+            setLoading(false);
+        }
+    }
 
     useEffect(() => {
-        async function handler() {
-            try {
-                const response = await orchestrateUserData();
-                setUserData(response);
-            } catch (e) {
-                logToConsole("Error accessing user data! " + e, "error", {
-                    location: "@/app/(tabs)/index.tsx",
-                    function: "handler()",
-                    isHandler: false,
-                });
-            } finally {
-                setLoading(false);
-            }
-        }
-
-        handler();
+        // load both user data and objectives at the same time
+        setLoading(true);
+        Promise.all([handler()]);
+        // eslint-disable-next-line
     }, []);
+
+    function generateDescriptionOfObjective(obj: ActiveObjective): string {
+        const exercise: SupportedActiveObjectives = obj.exercise;
+        if (exercise === "Lifting") {
+            return t(
+                obj.specificData.reps +
+                    " lifts of " +
+                    obj.specificData.dumbbellWeight *
+                        obj.specificData.amountOfHands +
+                    " kg each."
+            );
+        } else if (exercise === "Push Ups") {
+            return t(
+                obj.specificData.amountOfPushUps +
+                    " push ups with " +
+                    obj.specificData.amountOfHands +
+                    " hands."
+            );
+            // TODO: finish these
+        } else {
+            return "(There was an error reading this objective's data)";
+        }
+    }
+
+    async function handleMarkingObjectiveAsDone(identifier: number) {
+        await SaveActiveObjectiveToDailyLog(identifier, true, undefined);
+        // update & redraw without re-fetching everything upon update
+        setRenderedObjectives(
+            renderedObjectives?.filter(
+                (obj) => obj.identifier !== identifier
+            ) || null
+        );
+    }
 
     if (loading) return <Loading />;
 
@@ -49,7 +144,57 @@ export default function HomeScreen() {
             </BetterTextSubHeader>
             <GapView height={20} />
             <Section width="total" kind="ActiveObjectives">
-                <RenderActiveObjectives />
+                <>
+                    {identifiers === 0 && (
+                        <Division header="Guess you've done everything!" />
+                    )}
+
+                    {identifiers === null && (
+                        <Division header="You don't have any objectives... Let's create one!">
+                            <BetterButton
+                                style="GOD"
+                                action={() => router.push("/objectives/Create")}
+                                buttonText="Create active objective"
+                                buttonHint="Redirects the user to a page where he can create an active objective"
+                            />
+                        </Division>
+                    )}
+
+                    {identifiers === false && (
+                        <Division header="No objectives for today. Have a good rest!" />
+                    )}
+
+                    {renderedObjectives &&
+                        renderedObjectives.map((obj: ActiveObjective) => {
+                            return (
+                                <Division
+                                    key={obj.identifier}
+                                    header={obj.exercise}
+                                    preHeader="ACTIVE OBJECTIVE"
+                                    subHeader={generateDescriptionOfObjective(
+                                        obj
+                                    )}
+                                >
+                                    <BetterButton
+                                        buttonText="Let's go!"
+                                        buttonHint="Starts a session for the given objective"
+                                        style="ACE"
+                                        action={() => {}}
+                                    />
+                                    <BetterButton
+                                        buttonText="Already done it!"
+                                        buttonHint="Marks the given objective as done"
+                                        style="GOD"
+                                        action={() =>
+                                            handleMarkingObjectiveAsDone(
+                                                obj.identifier
+                                            )
+                                        }
+                                    />
+                                </Division>
+                            );
+                        })}
+                </>
             </Section>
             <PageEnd includeText={true} />
         </>
