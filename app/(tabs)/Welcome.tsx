adc085fa9e6f1c396f13e74811fa91c5db5e4904
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-10-23 20:59:40 +0200

    fix

--- app/(tabs)/Welcome.tsx
+++ app/(tabs)/Welcome.tsx
@@ -315,6 +315,7 @@ export default function WelcomePage() {
         return (
             <>
                 <BetterInputField
+                    readOnly={false}
                     label={label}
                     placeholder={placeholder}
                     value={value}
