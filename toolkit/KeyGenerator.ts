7a7459c95e4f189f34bdc6fba9635ead81fd5606
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-12-03 17:18:09 +0100

    Fix an issue causing too many re-renders and state loss

--- toolkit/KeyGenerator.ts
+++ toolkit/KeyGenerator.ts
@@ -1,5 +1,11 @@
-export default function GenerateRandomKey(item: string): string {
-    const n: number = Math.random() * 1000;
+// whatever this is, it works
+const keyCache = new Map<string, string>();
 
-    return `__PERSONAPLUS_${item}_ITEM__${n}__DO_NOT_REMOVE_OR_YOU_WILL_BE_FIRED`; // learning from react, heh~
+export default function GenerateRandomKey(item: string): string {
+    if (!keyCache.has(item)) {
+        const n: number = Math.random() * 1000;
+        const key = `__PERSONAPLUS_${item}_ITEM__${n}__DO_NOT_REMOVE_OR_YOU_WILL_BE_FIRED`;
+        keyCache.set(item, key); // learning from react, heh~
+    }
+    return keyCache.get(item)!;
 }
