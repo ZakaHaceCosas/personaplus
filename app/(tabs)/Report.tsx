7ea38b287ecd1e9152f4da8515eafb30e8feda22
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-12-03 17:42:13 +0100

    Improve Report data loading

--- app/(tabs)/Report.tsx
+++ app/(tabs)/Report.tsx
@@ -12,72 +12,35 @@ import Section from "@/components/ui/sections/Section";
 import CoreLibrary from "@/core/CoreLibrary";
 import { OrchestrateUserData } from "@/toolkit/User";
 import { logToConsole } from "@/toolkit/debug/Console";
-import { FullProfile } from "@/types/User";
 import { useEffect, useState } from "react";
 // import { useTranslation } from "react-i18next";
 
 export default function Report() {
     // const { t } = useTranslation();
-    const [userData, setUserData] = useState<FullProfile | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
     const [report, setReport] = useState<any>();
 
-    async function fetchUserData(): Promise<FullProfile> {
-        try {
-            const response: FullProfile | null = await OrchestrateUserData();
-            if (response === null) throw new Error("userData is null");
-            return response;
-        } catch (e) {
-            throw e;
-        }
-    }
-
     async function handler(): Promise<void> {
         try {
-            const data: FullProfile = await fetchUserData();
-            setUserData(data);
-        } catch (e) {
-            logToConsole("Error accessing user data! " + e, "error", {
-                location: "@/app/(tabs)/Report.tsx",
-                function: "fetchUserData()",
-                isHandler: true,
-                handlerName: "handler()",
-            });
-        } finally {
-            setLoading(false);
-        }
-    }
+            const data = await OrchestrateUserData();
 
-    useEffect((): void => {
-        handler();
-        // eslint-disable-next-line
-    }, []);
-
-    useEffect((): void => {
-        if (
-            userData?.age &&
-            userData?.gender &&
-            userData?.weight &&
-            userData?.height &&
-            userData?.activeness
-        ) {
             const BMISource =
                 CoreLibrary.physicalHealth.BodyMassIndex.calculate(
-                    userData.age,
-                    userData.gender,
-                    userData.weight,
-                    userData.height,
+                    data.age,
+                    data.gender,
+                    data.weight,
+                    data.height,
                 );
             const BMI: number = BMISource.result;
             const BMIContext: string | undefined = BMISource.context;
 
             const BMRSource =
                 CoreLibrary.physicalHealth.BasalMetabolicRate.calculate(
-                    userData.age,
-                    userData.gender,
-                    userData.weight,
-                    userData.height,
-                    userData.activeness,
+                    data.age,
+                    data.gender,
+                    data.weight,
+                    data.height,
+                    data.activeness,
                 );
             const BMR: number = BMRSource.result;
             const BMRContext: string | undefined = BMRSource.context;
@@ -92,8 +55,21 @@ export default function Report() {
                     context: BMRContext,
                 },
             });
+        } catch (e) {
+            logToConsole("Error handling your report! " + e, "error", {
+                location: "@/app/(tabs)/Report.tsx",
+                function: "fetchUserData()",
+                isHandler: true,
+                handlerName: "handler()",
+            });
+        } finally {
+            setLoading(false);
         }
-    }, [userData]);
+    }
+
+    useEffect((): void => {
+        handler();
+    }, []);
 
     if (loading) return <Loading />;
 
@@ -101,6 +77,14 @@ export default function Report() {
         <>
             <BetterTextHeader>Your report</BetterTextHeader>
             <BetterTextSubHeader>This is how you're doing</BetterTextSubHeader>
+            <GapView height={10} />
+            <BetterAlert
+                style="HMM"
+                preTitle="Disclaimer"
+                title="This page is an experiment"
+                layout="alert"
+                bodyText="This page is expected to change a lot. Until this warning is removed, please consider the values shown above imprecise and don't trust them."
+            />
             <GapView height={20} />
             <Section kind="HowYouAreDoing">
                 {report ? (
@@ -122,14 +106,6 @@ export default function Report() {
                     </BetterTextSmallText>
                 )}
             </Section>
-            <GapView height={20} />
-            <BetterAlert
-                style="HMM"
-                preTitle="Disclaimer"
-                title="This page is an experiment"
-                layout="alert"
-                bodyText="This page is expected to change a lot. Until this warning is removed, please consider the values shown above imprecise and don't use them for anything."
-            />
             <PageEnd includeText={true} />
         </>
     );
