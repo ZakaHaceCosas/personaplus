fa44d1def6232ab25eaf79273acf7038ae5fddf9
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-12-06 15:33:15 +0100

    Slightly increase lineHeight for all texts. ALso, JSDoc-ed BetterText

--- components/text/BetterText.tsx
+++ components/text/BetterText.tsx
@@ -1,5 +1,15 @@
-// src/BetterText.tsx
-// Text, con estilos apropiados y la tipografía Be Vietnam Pro
+/* <=============================================================================>
+ *  PersonaPlus - Give yourself a plus!
+ *  Copyright (C) 2024 ZakaHaceCosas and the PersonaPlus contributors. All rights reserved.
+ *  Distributed under the terms of the GNU General Public License version 3.0.
+ *  See the LICENSE file in the root of this for more details.
+ * <=============================================================================>
+ *
+ * You are in: @/components/text/BetterText.tsx
+ * Basically: `<Text>`, with preconfigured styling and fonts for usage within the app.
+ *
+ * <=============================================================================>
+ */
 
 import React, { ReactNode, ReactElement } from "react";
 import { TextStyle, Text } from "react-native";
@@ -7,17 +17,80 @@ import Colors from "@/constants/Colors";
 import { FontFamily, FontWeight } from "@/types/FontWeights";
 
 // TypeScript, supongo
+/**
+ * BetterTextProps
+ *
+ * @interface BetterTextProps
+ * @typedef {BetterTextProps}
+ * @template {FontFamily} [T="BeVietnamPro"]
+ */
 interface BetterTextProps<T extends FontFamily = "BeVietnamPro"> {
-    fontFamily?: T; // Font family
-    fontSize: number; // Tamaño de letra en px
-    fontWeight: FontWeight<T>; // Intensidad de la tipografía dependiente de fontFamily
-    textColor?: string; // Color del texto - default: blanco (ya que el fondo es negro)
-    onTap?: () => void; // Si se proporciona, el texto actuará como un botón / enlace y hará algo
-    textAlign?: "center" | "normal" | "left" | "right"; // Alineación del texto
-    children: ReactNode; // Texto en sí, dentro del tag <BeText>
-    isLink?: boolean; // Si es verdadero el texto se estilizará como un enlace externo - default: false
+    /**
+     * Font family. Defaults to BeVietnamPro. Only BeVietnamPro, Roboto Serif, and JetBrains Mono are supported.
+     *
+     * @type {?T}
+     */
+    fontFamily?: T;
+    /**
+     * Font size in px.
+     *
+     * @type {number}
+     */
+    fontSize: number;
+    /**
+     * Font weight. Can be "Regular", "Bold", etc...
+     *
+     * @type {FontWeight<T>}
+     */
+    fontWeight: FontWeight<T>;
+    /**
+     * Text color. Optional, defaults to pure white.
+     *
+     * @type {?string}
+     */
+    textColor?: string;
+    /**
+     * Optional, if passed, it will become the text's onTap.
+     *
+     * @type {?() => void}
+     */
+    onTap?: () => void;
+    /**
+     * Optional, aligns the text. Defaults to `"normal"`, which is equal to `"left"`.
+     *
+     * @type {?("center" | "normal" | "left" | "right")}
+     */
+    textAlign?: "center" | "normal" | "left" | "right";
+    /**
+     * The text itself, inside the `<BetterText>` tag. You can also pass other stuff, like another `<BetterText>` to act like a `<span>` would act like in HTML.
+     *
+     * @type {ReactNode}
+     */
+    children: ReactNode;
+    /**
+     * Optional, if true, text will be styled as if it was a clickable link.
+     *
+     * @type {?boolean}
+     */
+    isLink?: boolean;
 }
 
+/**
+ * A homemade text component with all needed props preconfigured.
+ *
+ * @export
+ * @template {FontFamily} [T="BeVietnamPro"]
+ * @param {BetterTextProps<T>} p
+ * @param {T} [p.fontFamily="BeVietnamPro" as T]
+ * @param {number} p.fontSize
+ * @param {FontWeight<T>} p.fontWeight
+ * @param {ReactNode} p.children
+ * @param {string} [p.textColor=Colors.BASIC.WHITE]
+ * @param {("center" | "normal" | "left" | "right")} [p.textAlign="normal"]
+ * @param {() => void} p.onTap
+ * @param {boolean} [p.isLink=false]
+ * @returns {ReactElement}
+ */
 export default function BetterText<T extends FontFamily = "BeVietnamPro">({
     fontFamily = "BeVietnamPro" as T,
     fontSize,
@@ -43,7 +116,7 @@ export default function BetterText<T extends FontFamily = "BeVietnamPro">({
     const textStyle: TextStyle = {
         fontFamily: fontFamilyName,
         fontSize: fontSize,
-        lineHeight: fontSize * 1.25,
+        lineHeight: fontSize * 1.3,
         overflow: "visible",
         color: isLink ? Colors.PRIMARIES.ACE.ACE : color,
         textAlign: alignment,
