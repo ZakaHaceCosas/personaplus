c845d57f1cc5d58bbb52efa430a8ff126b2f07c8
Zaka <zakahacecosas@protonmail.com>
2024-12-22 13:27:29 +0100

    (chore) Better document getCommonScreenSize)

--- constants/screen.ts
+++ constants/screen.ts
@@ -1,15 +1,22 @@
 import { Dimensions } from "react-native";
 
+/**
+ * Returns the common screen size (width or height) for all pages. Uses RN's Dimensions API and automatically adds padding to ensure content is visible when using these values.
+ *
+ * @export
+ * @param {("width" | "height")} request Whether you want the width or the height of the screen.
+ * @returns {number} The common screen size
+ */
 export default function getCommonScreenSize(
     request: "width" | "height",
 ): number {
-    const CommonWidth = Dimensions.get("screen").width - 40;
-    const CommonHeight = Dimensions.get("screen").height - 40 - 20;
-    /*
+    const CommonWidth: number = Dimensions.get("screen").width - 40;
+    const CommonHeight: number = Dimensions.get("screen").height - 40 - 20;
+    /**
     explanation:
     Dimensions - 40 (because of the padding of 20 that's applied by _layout to all pages)
     then height has an additional - 20 to compensate the extra 20 of padding thats applied to the top
     */
-
-    return request === "width" ? CommonWidth : CommonHeight;
+    const returnWidth = request === "width" ? CommonWidth : CommonHeight;
+    return returnWidth;
 }
