b6f1459cbb20dd62d3aae5baa05735ef6d254016
Zaka <zakahacecosas@protonmail.com>
2025-02-18 21:26:24 +0100

    (fix) all tests are passing less gooo

--- __tests__/README.md
+++ __tests__/README.md
@@ -2,4 +2,6 @@
 
 For CoreLibrary (CL) tests, go to `@/core/__tests__`.
 
-Note: As I am lazy to maintain this, most tests are failing. One day (soon) they'll get fixed, I promise...
+~~Note: As I am lazy to maintain this, most tests are failing. One day (soon) they'll get fixed, I promise...~~
+
+There are very few tests, but they all pass.
--- __tests__/toolkit/debug/console.test.ts
+++ __tests__/toolkit/debug/console.test.ts
@@ -1,12 +1,11 @@
-import AsyncStorage from "expo-sqlite/kv-store";
+import SyncStorage from "expo-sqlite/kv-store";
 import { getLogsFromStorage, logToConsole } from "@/toolkit/console";
-import { Logs } from "@/types/logs";
+import { Log } from "@/types/logs";
 import { ShowToast } from "@/toolkit/android";
 import StoredItemNames from "@/constants/stored_item_names";
 
 jest.mock("expo-sqlite/kv-store", () => ({
-    getItem: jest.fn(),
-    setItem: jest.fn(),
+    getItemSync: jest.fn(),
 }));
 
 jest.mock("react-native", () => ({
@@ -18,12 +17,12 @@ jest.mock("@/toolkit/android", () => ({
     ShowToast: jest.fn(),
 }));
 
-const mockGetItem = AsyncStorage.getItem as jest.MockedFunction<
-    typeof AsyncStorage.getItem
+const mockGetItemSync = SyncStorage.getItemSync as jest.MockedFunction<
+    typeof SyncStorage.getItemSync
 >;
 const mockShowToast = ShowToast as jest.MockedFunction<typeof ShowToast>;
 
-const sampleLogs: Logs = [
+const sampleLogs: Log[] = [
     {
         message: "hello chat",
         type: "log",
@@ -52,45 +51,45 @@ describe("logToConsole Toolkit", () => {
         jest.clearAllMocks();
     });
 
-    describe("getLogsFromStorage", () => {
+    describe("getLogsFromStorage", (): void => {
         // it should fetch objectives
-        test("should return logs from AsyncStorage", async () => {
-            mockGetItem.mockResolvedValueOnce(JSON.stringify(sampleLogs));
+        test("should return logs from AsyncStorage", (): void => {
+            mockGetItemSync.mockReturnValueOnce(JSON.stringify(sampleLogs));
 
-            const logs = getLogsFromStorage();
+            const logs: Log[] = getLogsFromStorage();
 
+            expect(mockGetItemSync).toHaveBeenCalledWith("globalLogs");
             expect(logs).toEqual(sampleLogs);
-            expect(AsyncStorage.getItem).toHaveBeenCalledWith("globalLogs");
         });
 
         // upon empty logs, empty array
-        test("should return an empty array if there are no logs", async () => {
-            mockGetItem.mockResolvedValueOnce(null);
+        test("should return an empty array if there are no logs", (): void => {
+            mockGetItemSync.mockReturnValueOnce(null);
 
-            const logs = getLogsFromStorage();
+            const logs: Log[] = getLogsFromStorage();
 
             expect(logs).toEqual([]);
-            expect(AsyncStorage.getItemSync).toHaveBeenCalledWith("globalLogs");
+            expect(mockGetItemSync).toHaveBeenCalledWith("globalLogs");
         });
 
         // upon error, empty array also
-        test("should return an empty array if there is an error", async () => {
-            mockGetItem.mockRejectedValueOnce(new Error("AsyncStorage error"));
+        test("should return an empty array if there is an error", (): void => {
+            mockGetItemSync.mockReturnValueOnce("non JSON string (error)");
 
-            const logs = getLogsFromStorage();
+            const logs: Log[] = getLogsFromStorage();
 
             expect(logs).toEqual([]);
-            expect(AsyncStorage.getItem).toHaveBeenCalledWith(
+            expect(mockGetItemSync).toHaveBeenCalledWith(
                 StoredItemNames.consoleLogs,
             );
         });
     });
 
-    describe("logToConsole", () => {
+    describe("logToConsole", (): void => {
         // these tests are all the same, it waits for console.* to be called with the same args that were passed to logToConsole. if that happens, it means it works.
         // on error / log with displayToEndUser=true, does the same with ToastAndroid
         // expect one console.error() warning by JEST when testing error logs, it is normal.
-        test("should log a message with 'log' type", () => {
+        test("should log a message with 'log' type", (): void => {
             const consoleLogSpy = jest
                 .spyOn(console, "log")
                 .mockImplementation();
@@ -100,7 +99,7 @@ describe("logToConsole Toolkit", () => {
             consoleLogSpy.mockRestore();
         });
 
-        test("should log a message with 'warn' type", () => {
+        test("should log a message with 'warn' type", (): void => {
             const consoleWarnSpy = jest
                 .spyOn(console, "warn")
                 .mockImplementation();
@@ -110,7 +109,7 @@ describe("logToConsole Toolkit", () => {
             consoleWarnSpy.mockRestore();
         });
 
-        test("should log a message with 'success' type", () => {
+        test("should log a message with 'success' type", (): void => {
             const consoleLogSpy = jest
                 .spyOn(console, "log")
                 .mockImplementation();
