0d72959f1b50e7600e07196b96a89d769309638c
Zaka <149817565+ZakaHaceCosas@users.noreply.github.com>
2025-03-10 09:29:26 +0100

    Fix TodaysDateRegularExpression
    Issue reported by GitHub

--- types/today.ts
+++ types/today.ts
@@ -32,7 +32,7 @@ export type TodaysDate = `${string}/${string}/${string}`;
  * @type {RegExp}
  */
 export const TodaysDateRegularExpression: RegExp = new RegExp(
-    "^([0-2][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$",
+    "^([0-2][0-9]|3[01])\/(0[1-9]|1[0-2])\/\\d{4}$",
 );
 
 /**
